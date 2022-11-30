6a0a948044efef4be9f5211ffb1801386e783cf3
andy235 <44841201+andy235@users.noreply.github.com>
2022-11-30 16:03:18 +0300

    Add files via upload

---
+++ autogen_json_serialization/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:autogen_json_serialization/offices.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter JSON demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, String? title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<OfficesList> officesList;
+
+  @override
+  void initState() {
+    super.initState();
+    officesList = getOfficesList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Manual JSON Serialization'),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<OfficesList>(
+        future: officesList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.offices.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text('${snapshot.data!.offices[index].name}'),
+                    subtitle:  Text('${snapshot.data!.offices[index].address}'),
+                    leading:  Image.network('${snapshot.data!.offices[index].image}'),
+                    isThreeLine: true,
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error');
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
